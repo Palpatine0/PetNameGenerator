3f8e606aeec331dd15c790ff4a540df86c505d46
Percival <emperorsheevpalpatine0@gmail.com>
2024-05-30 18:46:39 +0800

    feat: Added sidebar inputs in main.py for selecting pet type and entering pet color.

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import langchain_helper as lch
 import streamlit as st
 
+# Sets the title of the Streamlit web application to "Pet Name Generator".
 st.title("Pet Name Generator")
+
+# Creates a dropdown select box in the sidebar for choosing the type of pet, with options for Cat, Dog, Cow, and Hamster.
+animal_type = st.sidebar.selectbox("What is your pet?", ("Cat", "Dog", "Cow", "Hamster"))
+if animal_type == "Cat":
+    pet_color = st.sidebar.text_area(label = "What color is your cat?", max_chars = 15)
+
+if animal_type == "Dog":
+    pet_color = st.sidebar.text_area(label = "What color is your dog?", max_chars = 15)
+
+if animal_type == "Cow":
+    pet_color = st.sidebar.text_area(label = "What color is your cow?", max_chars = 15)
+
+if animal_type == "Hamster":
+    pet_color = st.sidebar.text_area(label = "What color is your hamster?", max_chars = 15)
