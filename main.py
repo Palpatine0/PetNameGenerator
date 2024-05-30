bf19a70dba34f1001fdc7e34f92ad6f5ced5b580
Percival <emperorsheevpalpatine0@gmail.com>
2024-05-30 18:54:08 +0800

    feat: Add Streamlit functionality to display generated pet names
    - Added functionality to display generated pet names based on user input in the main area of the Streamlit app.

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ import streamlit as st
 st.title("Pet Name Generator")
 
 # Creates a dropdown select box in the sidebar for choosing the type of pet, with options for Cat, Dog, Cow, and Hamster.
-animal_type = st.sidebar.selectbox("What is your pet?", ("Cat", "Dog", "Cow", "Hamster"))
-if animal_type == "Cat":
+user_animal_type = st.sidebar.selectbox("What is your pet?", ("Cat", "Dog", "Cow", "Hamster"))
+if user_animal_type == "Cat":
     pet_color = st.sidebar.text_area(label = "What color is your cat?", max_chars = 15)
 
-if animal_type == "Dog":
+if user_animal_type == "Dog":
     pet_color = st.sidebar.text_area(label = "What color is your dog?", max_chars = 15)
 
-if animal_type == "Cow":
+if user_animal_type == "Cow":
     pet_color = st.sidebar.text_area(label = "What color is your cow?", max_chars = 15)
 
-if animal_type == "Hamster":
+if user_animal_type == "Hamster":
     pet_color = st.sidebar.text_area(label = "What color is your hamster?", max_chars = 15)
+
+# Displays the generated response (suggested pet names) in the main area of the Streamlit app.
+if pet_color:
+    response= lch.generate_pet_name(user_animal_type, pet_color)
+    st.text(response)
